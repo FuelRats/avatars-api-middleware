d4efe028ec445c7a5203c02f0d24738360ccccc9
Cameron Welter <camjwelt@gmail.com>
2021-06-25 19:03:14 +0000

    Convert imaging into a workerpool for performance

--- src/lib/imaging.ts
+++ src/lib/imaging.ts
@@ -1,21 +1,27 @@
 import sharp, { FormatEnum } from 'sharp';
 import { parseSizeFactory } from 'avatars-utils';
 import { Face } from './FaceFactory';
+import workerpool from 'workerpool';
 
 const minSize = 32;
 const maxSize = 512;
 
-export const parseSize = parseSizeFactory(minSize, maxSize);
+const parseSize = parseSizeFactory(minSize, maxSize);
 
-export const resize = (rawSize: string): sharp.Sharp => {
+const resize = (rawSize: string): sharp.Sharp => {
   const size = parseSize(rawSize);
   return sharp().resize(size.width, size.height);
 };
 
-export const renderToBuffer = (face: Face, format: string): Promise<Buffer> =>
+const renderToBuffer = (face: Face, format: string): Promise<Buffer> =>
   sharp(face.eyes)
     .composite([{ input: face.mouth }, { input: face.nose }])
     .flatten({ background: face.color })
     .pipe(resize(face.size))
     .toFormat(format as keyof FormatEnum, { quality: 90 })
     .toBuffer();
+
+
+workerpool.worker({
+  renderToBuffer,
+});
